ac75d35e4c173228cd177e59ece00d99e0bee8f8
Maitry Parikh <140823694+maitry4@users.noreply.github.com>
2023-12-26 09:00:42 +0530

    First php script

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	echo "<b style='color:blue;'>Hello</b><br>","another,<br>";
+	echo "Good Mornig<br>";
+	print("Hello");
+
+	//echo is a statememt -generally used
+	//print is a statement
+?>
