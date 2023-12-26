cf85d28d0c6d1d34a7aedbfeacb3de2f4344b14c
Maitry Parikh <140823694+maitry4@users.noreply.github.com>
2023-12-26 11:27:08 +0530

    Created a registration form

---
+++ register.php
@@ -0,0 +1,28 @@
+<?php
+	echo" &nbsp; &nbsp;<h1>Register</h1>
+		<table>
+			<form>
+			<tr>
+				<td><label for='name'>Name: </label></td>
+				<td><input type='text' name='name' id='name'></td>
+			</tr>
+			<tr>
+				<td><label for='email'>Email: </label></td>
+				<td><input type='email' name='email' id='email'></td>
+			</tr>
+			<tr>
+				<td><label for='mobile'>Mobile No: </label>
+				<td><input type='number' name='mobile' id='mobile'></td>
+			</tr>
+			<tr>
+				<td><label for='city'>City: </label>
+				<td><input type='text' name='city' id='city'></td>
+			</tr>
+
+			<tr>
+				<td><label for='address'>Address: </label></td>
+				<td><textarea name='address'></textarea></td>
+			</tr>	
+			</form>
+		</table>";
+?>
