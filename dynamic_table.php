523a76d5a94b63edcae0065f5ac3a08305cd5344
Maitry Parikh <140823694+maitry4@users.noreply.github.com>
2024-01-16 08:40:17 +0530

    Update dynamic_table.php
    updated with some required changes like start value, end value, and correct formatting of the output.

--- dynamic_table.php
+++ dynamic_table.php
@@ -3,19 +3,27 @@
             <label for='num'>Enter a number: </label> 
             <input type='text' name='num' />
             <br />
+            <label for='start'>Enter Start Value: </label> 
+            <input type='text' name='start' />
+            <br />
+            <label for='end'>Enter End Value: </label> 
+            <input type='text' name='end' />
+            <br />
             <input type='submit' name='submit' />
         </form>";
        
         // Check if the form has been submitted using POST method
         if (isset($_REQUEST['submit']))
         {
-            if (isset($_REQUEST['num']))
+            if (isset($_REQUEST['num']) && isset($_REQUEST['start']) && isset($_REQUEST['end']))
             {
                 $number = $_REQUEST['num'];
-                for($i=1; $i<11; $i++)
+                $start = $_REQUEST['start'];
+                $end = $_REQUEST['end'];
+                for($i=$start; $i<=$end; $i++)
                 {
                     $res = $i * $number;
-                    echo "$i * $number = $res <br>";
+                    echo "$number * $i  = $res <br>";
                 }
             }
             else {
@@ -24,4 +32,3 @@
         }
         
 ?>
-
